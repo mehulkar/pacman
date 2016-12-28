40dfd0f86d27ce05009cdad961fdcaf07a2104db
mehulkar <mehul.kar@gmail.com>
2016-12-28 02:43:11 -0500

    init movement

---
+++ main.js
@@ -0,0 +1,76 @@
+// TODO get this from document element later
+// currently hard coded in CSS
+const MAX_RIGHT = 600;
+const MAX_LEFT = 0;
+const MAX_DOWN = 600;
+const MAX_UP = 0;
+
+const STEPSIZE = 10;
+
+const POSITIVE_DIRECTION = 1;
+const NEGATIVE_DIRECTION = -1;
+
+const MOVE_INTERVAL = 100;
+
+var KEY_MAP = {
+  37: { human: 'left',  axis: 'left', direction: NEGATIVE_DIRECTION, max: MAX_LEFT },
+  39: { human: 'right', axis: 'left', direction: POSITIVE_DIRECTION, max: MAX_RIGHT },
+  38: { human: 'up',    axis: 'top',  direction: NEGATIVE_DIRECTION, max: MAX_UP },
+  40: { human: 'down',  axis: 'top',  direction: POSITIVE_DIRECTION, max: MAX_DOWN },
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  const pacman = document.getElementById('pacman');
+
+  // move right on load
+  window.movementInterval = window.setInterval(
+    move.bind(this, pacman, KEY_MAP[39]),
+  MOVE_INTERVAL);
+
+  document.addEventListener('keydown', watchArrowKeyPresses);
+});
+
+function move(pacman, keyInfo) {
+
+  var moveAxis = keyInfo.axis;
+  var moveDirection = keyInfo.direction;
+  var max = keyInfo.max;
+
+  var currentPosition = +pacman.style[moveAxis].split('px')[0];
+  var diameter = pacman.offsetHeight; // this could also be width since it's a perfect circle
+
+  if (isOutOfBounds(currentPosition, max, moveDirection)) {
+    stopMovement();
+  } else {
+    var nextPos = currentPosition + (moveDirection * STEPSIZE);
+    pacman.style[moveAxis] = nextPos + 'px';
+  }
+}
+
+function stopMovement() {
+  console.log('Stopping game...');
+  window.clearInterval(window.movementInterval);
+  document.removeEventListener('keydown', watchArrowKeyPresses);
+}
+
+function watchArrowKeyPresses(event) {
+  const pacman = document.getElementById('pacman');
+
+  var keyInfo = KEY_MAP[event.keyCode]
+
+  if (!keyInfo) { return; }
+
+  window.clearInterval(window.movementInterval);
+  window.movementInterval = window.setInterval(
+    move.bind(this, pacman, keyInfo),
+  MOVE_INTERVAL);
+}
+
+
+function isOutOfBounds(current, max, direction) {
+  if (direction === 1) {
+    return current > max;
+  } else {
+    return current < max;
+  }
+}
